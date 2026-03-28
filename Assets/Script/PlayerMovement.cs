486cac3b00205bbd080478cb6aa7a3cae7d72ca2
Paulo Pereira <paulovvp@outlook.com>
2026-03-28 09:06:10 -0300

    Alteração na velocidade

--- Assets/Script/PlayerMovement.cs
+++ Assets/Script/PlayerMovement.cs
@@ -19,22 +19,22 @@ public class PlayerMovement : MonoBehaviour
     {
         if (Keyboard.current.wKey.isPressed)
         {
-            transform.position += new Vector3(0, 0.002f, 0);
+            transform.position += new Vector3(0, 0.02f, 0);
         }
 
         if (Keyboard.current.sKey.isPressed)
         {
-            transform.position += new Vector3(0, -0.002f, 0);
+            transform.position += new Vector3(0, -0.02f, 0);
         }
 
         if (Keyboard.current.aKey.isPressed)
         {
-            transform.position += new Vector3(-0.002f, 0, 0);
+            transform.position += new Vector3(-0.02f, 0, 0);
         }
 
         if (Keyboard.current.dKey.isPressed)
         {
-            transform.position += new Vector3(0.002f, 0, 0);
+            transform.position += new Vector3(0.02f, 0, 0);
         }
 
 
