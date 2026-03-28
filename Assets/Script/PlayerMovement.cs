1422fb86311cc77aa2da64f5eaa245d213e7bb21
Paulo Pereira <paulovvp@outlook.com>
2026-03-28 09:03:01 -0300

    Criação do movimento do jogador

--- Assets/Script/PlayerMovement.cs
+++ Assets/Script/PlayerMovement.cs
@@ -21,6 +21,21 @@ public class PlayerMovement : MonoBehaviour
             transform.position += new Vector3(0, 0.002f, 0);
         }
 
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position += new Vector3(0, -0.002f, 0);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position += new Vector3(-0.002f, 0, 0);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += new Vector3(0.002f, 0, 0);
+        }
+
 
     }
 
